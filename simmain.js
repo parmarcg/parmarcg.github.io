a593d6137363b0f635cfe8e17f725f17ac57c939
parmarcg <75427440+parmarcg@users.noreply.github.com>
2020-12-04 14:32:26 +0000

    Add files via upload

---
+++ simmain.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("canv");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(500, 500, 150, 0, 2 * Math.PI);
+ctx.lineWidth = 10;
+ctx.stroke();
+ctx.fillStyle = "#FF0000";
