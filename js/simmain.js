67b71d36edc394ab9ddd0f3258030a9ff0966076
parmarcg <75427440+parmarcg@users.noreply.github.com>
2020-12-07 11:39:06 +0000

    Update simmain.js

--- js/simmain.js
+++ js/simmain.js
@@ -1,8 +1,13 @@
-var canvas = document.getElementById("canv");
-var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(500, 500, 150, 0, 2 * Math.PI);
-ctx.lineWidth = 10;
-ctx.stroke();
-ctx.fillStyle = "#FF0000";
+ 
+
+program.init = function(canv){
+  var canvas = document.getElementById("canv");
+  var ctx = canvas.getContext("2d");
+
+  ctx.beginPath();
+  ctx.arc(500, 500, 150, 0, 2 * Math.PI);
+  ctx.lineWidth = 10;
+  ctx.stroke();
+  ctx.fillStyle = "#FF0000";
+}
