acbbb1bedbb7781ca49e6825636e82d9c439060f
parmarcg <75427440+parmarcg@users.noreply.github.com>
2020-12-29 18:19:57 +0000

    Update simmain.js

--- js/simmain.js
+++ js/simmain.js
@@ -1,41 +1,31 @@
-
-
 var dpi = window.devicePixelRatio || 1;
 var arx = 100;
 var ary = 100;
 var canvas = document.getElementById("canv");
-var ar = canvas.getContext("2d")
+var ar = canvas.getContext("2d");
 
 
 function resizeHandler(){
+//Autoresizes the canvas when the brower window changes dimentions, this avoids "squashing" the canvas
   canvas.height = canvas.offsetHeight;
   canvas.width = canvas.offsetWidth;
     }
 
-var publicApi = {};
-publicApi.initialize = function(p_canvas){
+var canvashandler = {};
+//sets up autoresizing
+canvashandler.initialize = function(p_canvas){
   canvas = p_canvas;
 }
+canvashandler.autoResize = function(){
 
-publicApi.autoResize = function(){
   window.onresize = function(){
     resizeHandler();
   };
     resizeHandler();
 }
 
+var calfreq = 100;
 
-
-function ppi() {
-  var style_height = +getComputedStyle(canvas).getPropertyValue("hi").slice(0, -2);
-  var style_width = +getComputedStyle(canvas).getPropertyValue("wi").slice(0, -2);
-
-  canvas.setAttribute('hi', style_height * dpi);
-  canvas.setAttribute('wi', style_width * dpi); 
-}
-ppi()
-
-moveCirc()
 
 function moveCirc() {
   arx = arx + 1;
@@ -57,7 +47,7 @@ function back(){
 
 function circ(){
   
-
+  
   ar.clearRect(0, 0, canvas.width, canvas.height);
   ar.beginPath();
   ar.arc(arx, ary, 10, 0, 2 * Math.PI);
@@ -69,3 +59,12 @@ function circ(){
 
 var body = "new"
 
+function initializesim(canvas){
+
+  moveCirc();
+
+
+}
+canvashandler.initialize(canvas);
+canvashandler.autoResize();
+initializesim();
