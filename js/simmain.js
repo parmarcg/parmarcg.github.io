da9d197170e8bda84fd65c5bf0adf29771cc7660
parmarcg <75427440+parmarcg@users.noreply.github.com>
2021-01-08 18:50:50 +0000

    Update simmain.js

--- js/simmain.js
+++ js/simmain.js
@@ -1,6 +1,6 @@
 var dpi = window.devicePixelRatio || 1;
 var arx = 50;
-var ary = 500;
+var ary = 50;
 var canvas = document.getElementById("canv");
 var ar = canvas.getContext("2d");
 var clicked = 0;
@@ -35,18 +35,20 @@ function recursiveCirc(){
   }, 1000/30);}
 	  
 function generate(){
-  circles.push(0)
+    circles.push([0])
+    //starting x value of objects, creating a 2d array entry for each object
 	document.getElementById("createbody").innerHTML = "add to " + clicked;
+	//changes the html to reflect how many objects have been created
 	clicked = clicked + 1;
-}; 
+	}; 
 
 function draw(){	
     console.log("Draw");
     ar.clearRect(0, 0, canvas.width, canvas.height);
     for (var i = 0; i < circles.length; i++) {
-      circles[i] += 5;
+      circles[i][0] += 5;
       ar.beginPath();
-      ar.arc(circles[i], ary, 100, 0, 2 * Math.PI);
+      ar.arc(circles[i][0], ary, 100, 0, 2 * Math.PI);
       ar.lineWidth = 5;
       ar.stroke();
       ar.fillStyle = "ffffff";
